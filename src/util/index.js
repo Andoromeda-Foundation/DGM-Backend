81a103d698d9656daff70784540ea2ed4afa4d30
Frank Wei <frank@frankwei.xyz>
2018-08-06 11:18:39 +0800

    good bye web3

--- src/util/index.js
+++ src/util/index.js
@@ -1,5 +1,3 @@
-const Web3 = require('web3')
-const web3 = new Web3()
 const ramda = require('ramda')
 const { splitEvery, compose, map } = ramda
 
@@ -12,8 +10,7 @@ function decodeHexedDataNumber(dataHex) {
     const remove0xPrefix = (str) => str.slice(2)
     const splitIntoHexArray = splitEvery(64)
     const add0x = str => (`0x${str.slice(2)}`)
-    const hexToDecimalStr = hex => web3.utils.toBN(hex).toString(10)
-
+    const hexToDecimalStr = hex => parseInt(hex, 16).toString()
     // (Ramda) Compose exec fns from the bottom to the top 😄, functional style
     const result = compose(
         map(hexToDecimalStr),
@@ -21,6 +18,7 @@ function decodeHexedDataNumber(dataHex) {
         splitIntoHexArray,
         remove0xPrefix,
     )(dataHex)
+    console.log(result)
     return result
 }
 
